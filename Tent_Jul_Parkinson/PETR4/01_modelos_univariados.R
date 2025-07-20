76a3aa3a8a56059019cdccf101d6663d6e32768e
ViniciusBardelin <125508443+ViniciusBardelin@users.noreply.github.com>
2025-07-20 12:11:37 -0300

    Update 01_modelos_univariados.R

--- Tent_Jul_Parkinson/PETR4/01_modelos_univariados.R
+++ Tent_Jul_Parkinson/PETR4/01_modelos_univariados.R
@@ -15,11 +15,9 @@ df$Data <- as.Date(df$Data)
 returns <- df$log_return
 N <- length(returns)
 n_ins <- 1500
+window_size <- 1500
 
-# ----------------------------
 # Especificações dos modelos
-# ----------------------------
-
 garch_spec <- ugarchspec(
   variance.model = list(model= "sGARCH", garchOrder = c(1,1)),
   mean.model = list(armaOrder  = c(0,0), include.mean = FALSE),
@@ -35,18 +33,15 @@ msgarch_spec <- CreateSpec(
 gas_spec <- UniGASSpec(
   Dist = "std",
   ScalingType = "Identity",
-  GASPar = list(scale = TRUE)
+  GASPar = list(locate = FALSE, scale = TRUE, shape = FALSE)
 )
 
-# ----------------------------
 # Valores ajustados e previsões
-# ----------------------------
-
 generate_sigma_hat_completo <- function(returns, model_type, window_size = 1500) {
   n <- length(returns)
   sigma_hat <- rep(NA_real_, n)
   
-  # --- centrando a série ---
+  # centrando a série
   initial_window <- returns[1:window_size]
   mu0 <- mean(initial_window, na.rm = TRUE)
   centered0 <- initial_window - mu0
@@ -59,15 +54,16 @@ generate_sigma_hat_completo <- function(returns, model_type, window_size = 1500)
   
   sigma_hat[1:window_size] <- switch(model_type,
                                      "garch"   = as.numeric(sigma(fit_tudo))^2,
-                                     "msgarch" = as.numeric(fitted(fit_tudo))^2,
-                                     "gas"     = {
-                                       nu <- fit_tudo@GASDyn$mTheta[3,1]
-                                       as.numeric(fitted(fit_tudo)) * nu/(nu-2)
+                                     "msgarch" = as.numeric(Volatility(fit_tudo))^2,
+                                     "gas" = {
+                                       vol_in_sample <- getMoments(fit_tudo)[, "M2"]
+                                       nu <- fit_tudo@GASDyn$mTheta[3, 1]
+                                       vol_in_sample * nu / (nu - 2)
                                      }
   )
-  
-  for (i in (window_size+1):n) {
-    w <- returns[(i-window_size):(i-1)]
+    
+  for (i in (window_size + 1):n) {
+    w <- returns[(i - window_size):(i - 1)]
     mu <- mean(w, na.rm = TRUE)
     wc <- w - mu
     
@@ -81,23 +77,20 @@ generate_sigma_hat_completo <- function(returns, model_type, window_size = 1500)
                            "garch"   = as.numeric(sigma(fit)[window_size])^2,
                            "msgarch" = (predict(fit, nahead = 1)$vol)^2,
                            "gas"     = {
-                             fc <- UniGASFor(fit, H = 1)
-                             nu <- fit@GASDyn$mTheta[3,1]
-                             fc@Forecast$PointForecast[2] * nu/(nu-2)
+                             forecast <- UniGASFor(fit, H = 1)
+                             nu <- fit@GASDyn$mTheta[3, 1]
+                             forecast@Forecast$PointForecast[2] * nu / (nu - 2)
                            }
     )
   }
   
-  sigma_hat
+  return(sigma_hat)
 }
 
-# ----------------------------
 # Gerar resultados
-# ----------------------------
-
 sigma_garch <- generate_sigma_hat_completo(returns, "garch", n_ins)
-#sigma_gas <- generate_sigma_hat_completo(returns, "gas", n_ins)
-#sigma_msgarch <- generate_sigma_hat_completo(returns, "msgarch", n_ins)
+sigma_msgarch <- generate_sigma_hat_completo(returns, "msgarch", n_ins)
+sigma_gas <- generate_sigma_hat_completo(returns, "gas", n_ins)
 
 # --- Validando só GARCH por enquanto --- #
 
@@ -122,7 +115,7 @@ resultados_garch <- resultados_garch %>%
   )
 
 # Exportar para CSV
-write.csv(resultados_garch, "vol_GARCH_1_1.csv", row.names = FALSE)
+#write.csv(resultados_garch, "vol_GARCH_1_1.csv", row.names = FALSE)
 
 # --- --- #
 
@@ -180,7 +173,7 @@ returns <- df$log_return
 N <- length(returns)
 window_size <- 1500
 
-# Médias
+# Salva as médias para calcular o VaR depois
 mu_vec <- rollapply(
   returns,
   width     = window_size,
@@ -197,3 +190,101 @@ out <- data.frame(
 )
 
 write.csv(out, "means_1500_garch_1_1.csv", row.names = FALSE)
+
+########### MSGARCH
+
+# --- Validando só MSGARCH por enquanto --- #
+
+# Criar dataframe com datas e sigma_garch
+df_msgarch <- data.frame(
+  Date = df$Data,
+  Sigma_MSGARCH = sigma_msgarch
+)
+
+resultados_msgarch <- data.frame(
+  Date = df$Data,
+  Returns = df$log_return,
+  Sigma_MSGARCH = sigma_msgarch
+)
+
+# Calcular resíduos
+resultados_msgarch <- resultados_msgarch %>%
+  mutate(
+    Residuals_msgarch = if_else(!is.na(Sigma_MSGARCH) & Sigma_MSGARCH > 0,
+                              Returns / sqrt(Sigma_MSGARCH),
+                              NA_real_)
+  )
+
+# Exportar para CSV
+write.csv(resultados_msgarch, "vol_MSGARCH_1_1.csv", row.names = FALSE)
+
+# PARKINSON
+dat  <- read.csv("vol_MSGARCH_1_1.csv", stringsAsFactors = FALSE)
+park <- read.csv("petr4_parkinson.csv", stringsAsFactors = FALSE)
+
+dat$Date  <- as.Date(dat$Date,  format = "%Y-%m-%d")
+park$Date <- as.Date(park$Date, format = "%Y-%m-%d")
+
+park_sub <- park[, c("Date", "Parkinson")]
+
+dat_merged <- merge(
+  dat,
+  park_sub,
+  by    = "Date",
+  all.x = TRUE
+)
+
+head(dat_merged)
+
+write.csv(dat_merged, "vol_MSGARCH_1_1.csv", row.names = FALSE)
+
+plot(dat_merged$Sigma_MSGARCH, type = 'l')
+
+## GAS
+
+# --- Validando só GAS por enquanto --- #
+
+# Criar dataframe com datas e sigma_gas
+df_gas <- data.frame(
+  Date = df$Data,
+  Sigma_GAS = sigma_gas
+)
+
+resultados_gas <- data.frame(
+  Date = df$Data,
+  Returns = df$log_return,
+  Sigma_GAS = sigma_gas
+)
+
+# Calcular resíduos
+resultados_gas <- resultados_gas %>%
+  mutate(
+    Residuals_gas = if_else(!is.na(Sigma_GAS) & Sigma_GAS > 0,
+                                Returns / sqrt(Sigma_GAS),
+                                NA_real_)
+  )
+
+# Exportar para CSV
+write.csv(resultados_gas, "vol_GAS_1_1.csv", row.names = FALSE)
+
+# PARKINSON
+dat  <- read.csv("vol_GAS_1_1.csv", stringsAsFactors = FALSE)
+park <- read.csv("petr4_parkinson.csv", stringsAsFactors = FALSE)
+
+dat$Date  <- as.Date(dat$Date,  format = "%Y-%m-%d")
+park$Date <- as.Date(park$Date, format = "%Y-%m-%d")
+
+park_sub <- park[, c("Date", "Parkinson")]
+
+dat_merged <- merge(
+  dat,
+  park_sub,
+  by    = "Date",
+  all.x = TRUE
+)
+
+head(dat_merged)
+
+write.csv(dat_merged, "vol_GAS_1_1.csv", row.names = FALSE)
+
+plot(df_gas$Sigma_GAS, type = 'l')
