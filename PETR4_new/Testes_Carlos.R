59b26adaa198b77ba89c1408e7148f8b5c197e09
ViniciusBardelin <125508443+ViniciusBardelin@users.noreply.github.com>
2025-08-17 19:52:48 -0300

    Update Testes_Carlos.R

--- PETR4_new/Testes_Carlos.R
+++ PETR4_new/Testes_Carlos.R
@@ -3,36 +3,36 @@ library(MSGARCH)
 library(GAS)
 library(dplyr)
 library(tidyr)
-
+library(arfima)
+library(tseries)
+library(FinTS)
+library(fracdiff)
+library(fEGarch)
 
 df <- read.csv("petr4_returns.csv")
 df$Date <- as.Date(df$Date)
 returns <- df$log_return
 
-
 n_ins <- 1500
 n_tot <- length(returns)
 n_oos <- n_tot - n_ins
-
 
 garch_spec<- ugarchspec(variance.model = list(model= "sGARCH", garchOrder = c(1,1)), mean.model = list(armaOrder  = c(0,0), include.mean = FALSE), distribution.model = "std")
 msgarch_spec <- CreateSpec(variance.spec = list(model = "sGARCH"), distribution.spec = list(distribution = "std"), switch.spec = list(do.mix = FALSE, K = 2))
 gas_spec <- UniGASSpec(Dist = "std", ScalingType = "Identity", GASPar = list(scale = TRUE))
 
 #InS
-
-# Matriz final com valores ajustados + previsões (variância sigma²)
 sigma2_completo <- matrix(NA_real_, nrow = n_tot, ncol = 3,
                           dimnames = list(NULL, c("GARCH", "MSGARCH", "GAS")))
 
-# Ajuste único nos 1500 primeiros dados centrados
+# Ajuste único nos n_ins primeiros dados centrados
 returns_c <- scale(returns[1:n_ins], scale = FALSE)
 
 fit_GARCH <- ugarchfit(garch_spec, returns_c, solver = "hybrid")
 fit_GAS <- UniGASFit(gas_spec, returns_c, Compute.SE = FALSE)
 fit_MSGARCH <- FitML(msgarch_spec, returns_c, ctr = list(do.se = FALSE))
 
-# Valores ajustados (sigma²) nas primeiras 1500 posições
+# Valores ajustados (sigma²) nas primeiras n_ins posições
 sigma2_completo[1:n_ins, "GARCH"] <- sigma(fit_GARCH)^2
 sigma2_completo[1:n_ins, "GAS"] <- fit_GAS@GASDyn$mTheta[2, 1:n_ins] * fit_GAS@GASDyn$mTheta[3, 1] / (fit_GAS@GASDyn$mTheta[3, 1] - 2)
 sigma2_completo[1:n_ins, "MSGARCH"] <- Volatility(fit_MSGARCH)^2
@@ -97,10 +97,6 @@ for (i in 1:n_oos) {
   
 }
 
-mean(r_oos < VaR_1[, "GARCH"])
-mean(r_oos < VaR_1[, "GAS"])
-mean(r_oos < VaR_1[, "MSGARCH"])
-
 # InS
 df_sigma2_completo <- data.frame(
   Date = df$Date,
@@ -146,3 +142,69 @@ df_oos <- data.frame(
 )
 
 write.csv(df_oos, "previsoes_oos_vol_var_es.csv", row.names = FALSE)
+
+# ARFIMA
+df_arf_rugarch = read.csv("sigma2_ajustado_e_previsto_completo.csv")
+
+pks_insample <- df_arf_rugarch$Parkinson[1:1500]
+
+sigma2_pks_rugarch <- matrix(NA, nrow = n_oos, ncol = 1)
+colnames(sigma2_pks_rugarch) <- "ARFIMA"
+
+sigma2_completo_pks_rugarch <- matrix(NA, nrow = n_tot, ncol = 1)
+colnames(sigma2_completo_pks_rugarch) <- "ARFIMA"
+
+# InS
+fit_autoarfima = autoarfima(data = pks_insample, ar.max = 2, ma.max = 2,
+                 criterion = c("AIC"), method = "partial", arfima = TRUE,
+                 distribution.model = "std") # ARFIMA(2,d,2)
+
+arfima_spec <- arfimaspec(
+  mean.model = list(armaOrder = c(2, 2), include.mean = FALSE),
+  arfima = TRUE,
+  distribution.model = "std"
+)
+
+fit_arfima_ins <- arfimafit(spec = arfima_spec, data = pks_insample)
+fitted_arfima_ins_rugarch <- fitted(fit_arfima_ins)
+
+rets <- returns[1:1500]
+vol_arfima <- sqrt(fitted_arfima_ins_rugarch)
+vol_arfima[!is.finite(vol_arfima) | vol_arfima == 0] <- NA
+res_arfima_rugarch_manual <- rets / (vol_arfima)
+res_arfima_rugarch_manual <- na.omit(res_arfima_rugarch_manual)
+
+df_arfima_insample_pks <- data.frame(
+  Date = df$Date[1:n_ins],
+  Sigma2_ARFIMA_ajustado = fitted_arfima_ins_rugarch,
+  Residuals = res_arfima_rugarch_manual
+)
+
+write.csv(df_arfima_insample_pks, "df_arfima_insample_pks_2.csv", row.names = FALSE)
+
+# OoS
+pks_oos <- df$Parkinson
+sigma2_pks <- data.frame(ARFIMA = rep(NA_real_, n_oos))
+for (i in 1:n_oos) {
+  cat("Iteração:", i, "\n")
+  
+  pks_window <- pks_oos[i:(i + n_ins - 1)]
+
+  fit_best <- try(arfimafit(spec = arfima_spec, data = pks_window), silent = TRUE)
+  
+  if (!inherits(fit_best, "try-error") && fit_best@fit$convergence == 0) {
+    pred <- arfimaforecast(fit_best, n.ahead = 1)
+    pks_hat <- as.numeric(pred@forecast$seriesFor[1])
+    sigma2_pks[i, "ARFIMA"] <- pks_hat
+  } else {
+    warning(paste("Erro de ajuste na iteração", i))
+  }
+}
+
+df_arfima_oos <- tibble(
+  Date = df$Date[(n_ins + 1):(n_ins + n_oos)],
+  Parkinson = df$Parkinson[(n_ins + 1):(n_ins + n_oos)],
+  Sigma2_ARFIMA = sigma2_pks[, "ARFIMA"]
+)
+
+write.csv(df_arfima_oos, "df_arfima_oos_pks_2.csv", row.names = FALSE)
